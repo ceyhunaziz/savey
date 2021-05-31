b011becff8fa0a0a6c6ab6ba99e7b796f5ba159a
ceyhunaziz <jeyhun.azizzade@pashabank.az>
2021-06-01 01:17:54 +0400

    SV-3/Implement on service level

--- src/main/java/az/savey/ms/savey/service/CreditService.java
+++ src/main/java/az/savey/ms/savey/service/CreditService.java
@@ -2,9 +2,11 @@ package az.savey.ms.savey.service;
 
 import az.savey.ms.savey.dao.CreditRepository;
 import az.savey.ms.savey.dao.model.CreditEntity;
+import az.savey.ms.savey.exception.CreditException;
+import az.savey.ms.savey.exception.StatusCode;
 import az.savey.ms.savey.mapper.CreditMapper;
+import az.savey.ms.savey.model.BaseResponse;
 import az.savey.ms.savey.model.request.CreditRequestDto;
-import az.savey.ms.savey.model.response.CreditResponseDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,15 +19,30 @@ public class CreditService {
         this.creditRepository = creditRepository;
     }
 
-    public CreditResponseDto createCredit(CreditRequestDto credit) {
+    public BaseResponse createCredit(CreditRequestDto credit) {
         CreditEntity creditEntity = CreditMapper.INSTANCE.creditRequestToEntity(credit);
         creditRepository.save(creditEntity);
 
-        return CreditMapper.INSTANCE.creditEntityToResponse(creditEntity);
+        return getResponse(CreditMapper.INSTANCE.creditEntityToResponse(creditEntity));
     }
 
-    public List<CreditResponseDto> getCredits() {
+    public BaseResponse getCredits() {
         List<CreditEntity> credits = creditRepository.findAllByIsActiveIsTrue();
-        return CreditMapper.INSTANCE.bulkCreditEntityToResponse(credits);
+
+        return getResponse(CreditMapper.INSTANCE.bulkCreditEntityToResponse(credits));
+    }
+
+    public BaseResponse getCreditById(Long id) {
+        CreditEntity creditEntity = creditRepository.findByIdAndIsActiveIsTrue(id).orElseThrow(
+                () -> new CreditException("No credit with this id")
+        );
+
+        return getResponse(CreditMapper.INSTANCE.creditEntityToResponse(creditEntity));
+    }
+
+    private BaseResponse getResponse(Object response) {
+        BaseResponse baseResponse = new BaseResponse(StatusCode.SUCCESS);
+        baseResponse.setData(response);
+        return baseResponse;
     }
 }
